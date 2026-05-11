413ef2006640f55a87758ff6772fd5aa6e91b7e1
xander <alexgv2122@gmail.com>
2026-05-11 19:20:13 -0400

    adjust vectorizer

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ def splitData(x, y):
     return xTrain, xVal, xTest, yTrain, yVal, yTest
 
 def extractFeatures(xTrain, xVal, xTest):
-    tfidf = TfidfVectorizer(max_features=5000)
+    tfidf = TfidfVectorizer(max_features=10000, ngram_range=(1, 2))
 
     xTrainTfidf = tfidf.fit_transform(xTrain)
     xValTfidf = tfidf.transform(xVal)
